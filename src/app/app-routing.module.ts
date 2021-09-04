fe2dca323b97bc560337e4bc3dc7e68cf2c98107
Lucas Barros <lucasbs1991@gmail.com>
2021-09-04 12:45:23 -0300

    feat: adding routes for all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { PlataformasComponent } from './plataformas/plataformas.component'
+import { PlanosComponent } from './planos/planos.component'
+import { FormularioComponent } from './formulario/formulario.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PlataformasComponent },
+  { path: 'planos', component: PlanosComponent },
+  { path: 'formulario', component: FormularioComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+
+import { PlataformasComponent } from './plataformas/plataformas.component'
+import { PlanosComponent } from './planos/planos.component'
+import { FormularioComponent } from './formulario/formulario.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanosComponent,
+    PlataformasComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
